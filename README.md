dc2ba85cdd489675002c4a238945d1cb492927a6
sonja-ops <sodju@hotmail.com>
2025-03-27 10:02:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# undervisning-api-27.03
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sonja-ops/undervisning-api-27.03)
---
+++ script.js
@@ -0,0 +1,33 @@
+//OPPGAVE 1:
+// Steg 1: Lag en async funskjon som heter fetchData
+// Steg 2: Lag en variabel som bruker await for å ta i mot en array med objects
+// Steg 3: Retuner den variabelen
+
+// OPPGAVE 2:
+// Steg 1: Lag en async funskjon som heter dataRecieved
+// Steg 2: Lag en variabel som bruker await for å ta i mot funksjonen getData
+// Steg 3: Logg ut denne dataen
+// Steg 4: Legg til denne dataen på siden
+
+//
+//
+
+// HER KODER VI SANNTID SAMMEN FOR CAT FACTS:
+
+// API-link: https://catfact.ninja/facts
+
+// Fetcher data fra catFact
+
+// Parser til JSON
+// Hele arrayet
+// Vi finner data array, array med objects
+// Vi velger et spesifikt array
+// Vi finner fact
+
+//Lagre array av objects i en variabel
+
+//forEach for å få ut hver fact på siden
+
+//VI LAGER EN CHUCK NORRIS KNAPP
+
+//API-link: https://api.chucknorris.io/jokes/random
